c171ca9127297bee7021eeab05b8d5beaea1cd47
batatinhoProGamer <bbatata686@gmail.com>
2023-04-22 14:38:53 -0300

    Utilizando git

---
+++ Fundamentos/EstruturaCondicional/EstruturaCondicionalAnd.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos.EstruturaCondicional
+{
+    internal class EstruturaCondicionalAnd
+    {
+        public void Executar()
+        {
+            double peso = 80;
+            double altura = 1.76;
+            double imc = peso / Math.Pow(altura, 2);
+            bool a = true || false;
+            string textoIMC = "";
+            if (imc < 18.5)
+            {
+                textoIMC = "Abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                textoIMC = "Peso ideal";
+            }
+            else if (imc < 30)
+            {
+                textoIMC = "Levemente acima do peso";
+            }
+            else if (imc < 35)
+            {
+                textoIMC = "Obesidade I";
+            }
+            else if (imc <= 40)
+            {
+                textoIMC = "Obesidade II";
+            }
+            else
+            {
+                textoIMC = "Obesidade III";
+            }
+            Console.WriteLine(textoIMC);
+        }
+    }
+}
---
+++ Fundamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using Fundamentos.ApresentarDados;
+using Fundamentos.EntradaDados;
+using Fundamentos.EstruturaCondicional;
+
+// ApresentarDados01 executor = new ApresentarDados01();
+// executor.Executar();
+
+//EntradaDados01 executor = new EntradaDados01();
+//executor.Executar();
+
+//EstruturaCondicionalOr executor = new EstruturaCondicionalOr();
+//executor.Executar();
+
+EstruturaCondicionalAvancada executor = new EstruturaCondicionalAvancada();
+executor.Executar();
