c3d579597b9aedb857f2b139a8753268c9718990
batatinhoProGamer <bbatata686@gmail.com>
2023-06-24 15:57:49 -0300

    Criado classes referente aos fluxos do cadastro do Cliente

---
+++ WindowsFormsExemplos/Forms/Clientes/ListagemClienteForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsExemplos.Forms.Clientes
+{
+    public partial class ListagemClienteForm : Form
+    {
+        public ListagemClienteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
